ed0fb26cdb5ab7f8dba873383ce76e4b146b153f
ing.conti <ingconti@ingconti.com>
2023-04-08 22:09:11 +0200

    added God.java with ENUM

--- src/main/java/com/ingconti/God.java
+++ src/main/java/com/ingconti/God.java
@@ -1,14 +1,66 @@
+//God.java
+
 package com.ingconti;
 
-public class God {
-    String name;
-    String description;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+class God {
+
+    enum PowerType {
+        CAN_THROW_LIGHTINGS,
+        CAN_RAISE_WAVES,
+        CAN_AWAKE_DEAD,
+    }
+    private String name;
+    private String desc;
+    private PowerType powerType;
+
+    public God( String name, String desc, PowerType powerType) {
+        this.name = name;
+        this.desc = desc;
+        this.powerType = powerType;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public PowerType getPowerType() {
+        return powerType;
+    }
+
+    public void setPowerType(PowerType powerType) {
+        this.powerType = powerType;
+    }
 
     @Override
     public String toString() {
-        return "God:" +
-                name + " " +
-                description;
+        return "God{" +
+                "name='" + name + '\'' +
+                ", desc='" + desc + '\'' +
+                ", powerType=" + powerType +
+                '}';
+    }
+
+
+    public  String JSONString(){
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String result = gson.toJson(this);
+        return result;
     }
 }
-
--- src/test/java/com/ingconti/AppTest.java
+++ src/test/java/com/ingconti/AppTest.java
@@ -1,6 +1,8 @@
 package com.ingconti;
 
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -10,14 +12,18 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void godTest()
     {
-        assertTrue( true );
+        God god = new God( "Neptune", "God of Sea", God.PowerType.CAN_RAISE_WAVES);
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String result = gson.toJson(god);
+        assertTrue( result.contains(  "\"powerType\": \"CAN_RAISE_WAVES\"") );
+
+        String jsonStr = god.JSONString();
+        assertTrue( result.contains(  "\"powerType\": \"CAN_RAISE_WAVES\"") );
+
     }
-    @Test
-    public void shouldAnswerWithTrueAgain()
-    {
-        assertTrue( true );
-    }
+
 
 }
